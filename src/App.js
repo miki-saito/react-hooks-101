00c84355fb7bfac29a711551c76e1bc7c1353ceb
miki-saito <65391589+miki-saito@users.noreply.github.com>
2020-09-09 11:34:46 +0900

    implement callbacks with useEffect

--- src/App.js
+++ src/App.js
@@ -13,19 +13,12 @@ const App = props => {
   }, [])
 
   useEffect(() => {
-    console.log('This callback is for name only.');
+    console.log('This callback is like componentDidMount.');
   }, [name])
-
-  // const renderPeriod = () => {
-  //   console.log('renderPeriod renders period.');
-  //   return '。'
-  // }
 
   return (
     <>
-    <p>現在の{name}は、{price}円です。
-    {/* {renderPeriod()} */}
-    </p>
+    <p>現在の{name}は、{price}円です。</p>
     <button onClick={() => setState({...state, price: state.price + 1})}>+1</button>
     <button onClick={() => setState({...state, price: state.price - 1})}>-1</button>
     <button onClick={() => setState(props)}>Reset</button>
@@ -33,21 +26,6 @@ const App = props => {
     </>
   )
 }
-
-// リファクタリングなし
-// const App = props => {
-//   const [state, setState] = useState(props)
-
-//   return (
-//     <>
-//     <p>現在の{state.name}は、{state.price}円です。</p>
-//     <button onClick={() => setState({...state, price: state.price + 1})}>+1</button>
-//     <button onClick={() => setState({...state, price: state.price - 1})}>-1</button>
-//     <button onClick={() => setState(props)}>Reset</button>
-//     <input value={state.name}　onChange={e => setState({...state, name: e.target.value })}/>   {/* リセットボタンおよび処理 */}
-//     </>
-//   )
-// }
 
 App.defaultProps = {
   name:'',
