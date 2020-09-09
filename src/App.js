77f1dd0f7bcc302d5797fd61eea492b58596ffec
miki-saito <65391589+miki-saito@users.noreply.github.com>
2020-09-09 10:28:39 +0900

    implement object state

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 import React, { useState } from 'react';
-// 内部に情報を持たせるなら
-// const App = () => {
-//   const initialStates = {
-//     name:'',
-//     price: 1000
-//   } //以下つづく
 
 const App = props => {
-  const [name, setName] = useState(props.name)
-  const [price, setPrice] = useState(props.price)
-  
-  const reset = () => {
-    setPrice(props.price)
-    setName(props.name)
-  }
+  const [state, setState] = useState(props)
+  const { name, price } = state
+
   return (
     <>
     <p>現在の{name}は、{price}円です。</p>
-    <button onClick={() => setPrice(price + 1)}>+1</button>
-    <button onClick={() => setPrice(price - 1)}>-1</button>
-    <button onClick={() => setPrice(reset)}>Reset</button>
-    <input value={name}　onChange={e => setName(e.target.value)}/>
+    <button onClick={() => setState({...state, price: state.price + 1})}>+1</button>
+    <button onClick={() => setState({...state, price: state.price - 1})}>-1</button>
+    <button onClick={() => setState(props)}>Reset</button>
+    <input value={name}　onChange={e => setState({...state, name: e.target.value })}/>   {/* リセットボタンおよび処理 */}
     </>
   )
 }
+
+// リファクタリングなし
+// const App = props => {
+//   const [state, setState] = useState(props)
+
+//   return (
+//     <>
+//     <p>現在の{state.name}は、{state.price}円です。</p>
+//     <button onClick={() => setState({...state, price: state.price + 1})}>+1</button>
+//     <button onClick={() => setState({...state, price: state.price - 1})}>-1</button>
+//     <button onClick={() => setState(props)}>Reset</button>
+//     <input value={state.name}　onChange={e => setState({...state, name: e.target.value })}/>   {/* リセットボタンおよび処理 */}
+//     </>
+//   )
+// }
+
 App.defaultProps = {
   name:'',
   price: 1000
