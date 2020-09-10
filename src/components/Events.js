f17bdc19490400b7f8103e2372fa36330dadc208
miki-saito <65391589+miki-saito@users.noreply.github.com>
2020-09-10 09:54:53 +0900

    setup react context provider

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Event from  './Event'
+import AppContext from '../contexts/AppContexts'
 
 const Events = ({ state, dispatch }) => {
+    const value = useContext(AppContext)
+
     return (
         <>
             <h4>イベント一覧</h4>
